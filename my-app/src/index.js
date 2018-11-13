923681562c50397c4fe9066799ddeba7adff2b23
Ayman Amoodi <aamoodi@horizon.csueastbay.edu>
2018-11-13 00:03:39 -0800

    HW8

---
+++ my-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(<div>
+        <h1>Hello CS651 from React</h1>
+        <h2>This is: Ayman Amoodi</h2>
+        <h2>NetID: UE3673</h2>
+    </div>, document.getElementById('root'));
