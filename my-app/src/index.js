1aa0b581834279c716d143251cc07ebc44d846fe
Ayman Amoodi <aamoodi@horizon.csueastbay.edu>
2018-11-13 00:50:30 -0800

    HW8

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import QandA from './QandA.json';
 ReactDOM.render(<div>
         <h1>Hello CS651 from React</h1>
         <h2>This is: Ayman Amoodi</h2>
         <h2>NetID: UE3673</h2>
+        {QandA.map((QDetails, index)=>{
+          return<div><h2>{QDetails.question}</h2>
+          <h2>{QDetails.answer}</h2></div>
+        })}
     </div>, document.getElementById('root'));
